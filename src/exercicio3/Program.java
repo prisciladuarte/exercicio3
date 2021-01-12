ce6753efbe57adb73bfa4c7d8c1fc264dce1abbc
Mabia <49458473+prisciladuarte@users.noreply.github.com>
2021-01-12 16:18:54 -0300

    Initial commit

---
+++ src/exercicio3/Program.java
@@ -0,0 +1,26 @@
+package exercicio3;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D, dif;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		dif = A * B - C * D;
+		
+		System.out.println("DIFERENCA = " + dif );
+		
+		sc.close();
+
+	}
+
+}
